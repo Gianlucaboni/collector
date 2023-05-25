8e4256955e31e7247c750dd67aedef83477f049b
Gianlucaboni <Gianluca>
2023-05-25 17:04:28 +0100

    new tmux runner

--- brasil/run.sh
+++ brasil/run.sh
@@ -4,6 +4,5 @@ tmux new -d -s brazil
 tmux kill-session -t brazil2
 tmux new -d -s brazil2
 tmux send -t brazil "conda activate cp" ENTER
-tmux send -t brazil "python3 collector_br.py" ENTER
-tmux send -t brazil2 "conda activate cp" ENTER
-tmux send -t brazil2 "python3 collector_br2.py" ENTER
+tmux send -t brazil "python collector_br.py -csv ./cities.csv -o brasil1.txt" ENTER
+tmux send -t brazil2 "python collector_br.py -csv ./cities2.csv -o brasil2.txt" ENTER
