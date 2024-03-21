55cf1e459ed7c70876b9068d7f582cfe29fe84bb
GianlucaBoni <gianlucaboni@live.it>
2024-03-21 05:51:49 +0000

    activating env

--- brasil/run.sh
+++ brasil/run.sh
@@ -4,5 +4,6 @@ tmux new -d -s brazil
 tmux kill-session -t brazil2
 tmux new -d -s brazil2
 tmux send -t brazil "conda activate cp" ENTER
+tmux send -t brazil2 "conda activate cp" ENTER
 tmux send -t brazil "python collector_br.py -csv ./cities.csv -o brasil1.txt" ENTER
 tmux send -t brazil2 "python collector_br.py -csv ./cities2.csv -o brasil2.txt" ENTER
